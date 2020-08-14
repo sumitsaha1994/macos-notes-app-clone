71977efd6c6185b13c111aa79fa95ca3e3cb36f3
Sumit saha <smtsh9@gmail.com>
2020-08-14 16:39:06 +0530

    prod setup

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+console.log(process.env.PORT);
+app.listen(process.env.PORT || 9000, () => {
+    console.log(`Listening to port ${process.env.PORT || 9000}`);
+});
